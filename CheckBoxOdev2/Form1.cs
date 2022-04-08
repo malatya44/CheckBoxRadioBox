f4ae066fb17e5e68012b7f97b481b20f1b781c7b
malatya44 <malatya44@gmail.com>
2022-04-08 22:13:59 +0300

    Yorum satırında düzeltme yapıldı.

--- CheckBoxOdev2/Form1.cs
+++ CheckBoxOdev2/Form1.cs
@@ -31,7 +31,7 @@ namespace CheckBoxOdev2
         {
             listBox1.Items.Clear();
             //şehirler
-            // deneme ercan
+            // deneme Ertuğrul
             foreach (Control rb in groupBox2.Controls)
             {
                 if (((RadioButton)rb).Checked)
